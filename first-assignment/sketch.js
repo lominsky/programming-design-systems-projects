a92b9b16c0a9f31d0d3b6571d50380d2a283d65e
Louis Minsky <louis@louisminsky.com>
2016-01-26 12:19:24 -0500

    Ice Cream Cone!

--- first-assignment/sketch.js
+++ first-assignment/sketch.js
@@ -5,16 +5,34 @@ var r = new Rune({
   debug: true
 });
 
-r.rect(100, 100, 200, 200)
-  .fill(255, 0, 0)
-  .stroke(0)
+r.rect(0, 0, 800, 800)
+  .fill(0)
+  .stroke(false)
 
-r.triangle(400, 50, 400, 400, 650, 50)
-  .fill(0, 255, 0)
-  .stroke(0)
+for(var i = 0; i < 3; i++) 
+{
+  var x, y, c;
+  if(i == 0)
+  {
+    x = 350;
+    y = 300;
+    c = 255;
+  } else if(i == 1) {
+    x = 400;
+    y = 190;
+    c = 255;
+  } else {
+    x = 450;
+    y = 280;
+    c = 255;
+  }
+  r.ellipse(x, y, 200, 200)
+    .fill(c)
+    .stroke(false)
+}
 
-r.ellipse(200, 500, 200, 300)
-  .fill(0, 0, 255)
+r.triangle(300, 350, 500, 350, 400, 710)
+  .fill(255)
   .stroke(false)
 
 r.draw();
