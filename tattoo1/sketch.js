986d6a6dae2bf000dc7e669e20959a48e6164275
Louis Minsky <github@louisminsky.com>
2016-04-26 11:20:28 -0400

    doing stuff

--- tattoo1/sketch.js
+++ tattoo1/sketch.js
@@ -9,13 +9,15 @@ var shiftX = 150;
 var shiftY = 200;
 
 var count = 5;
+var width = 5
 
 function draw() {
 	r.stage.children = [];
 
 	for(var i = 0; i < count; i++)
 		t(400, 300, i*360/count);
-	 
+	
+	text();
 	r.draw();
 }
 
@@ -24,7 +26,7 @@ function t(topX, topY, rotation) {
 	var tri = r.triangle(topX, topY, topX+shiftX, topY+shiftY, topX-shiftX, topY+shiftY)
 		.fill(false)
 		.stroke(0, 0, 0)
-		.strokeWidth(5)
+		.strokeWidth(width)
 		.rotate(rotation, topX, centerY);
 }
 
@@ -48,4 +50,28 @@ function decreaseWidth() {
 	if(shiftX > 10)
 		shiftX -= 10;
 	draw();
+}
+
+function increaseStroke() {
+	width += 1;
+	draw();
+}
+
+function decreaseStroke() {
+	if(width > 1)
+		width -= 1;
+	draw();
+}
+
+function text() {
+	document.getElementById("numTris").innerHTML = count + " Triangles";
+	document.getElementById("baseWidth").innerHTML = "Base width is " + shiftX;
+	document.getElementById("strokeWidth").innerHTML = "Stroke width is " + width;
+}
+
+function download() {
+	var e = document.createElement('script');
+	e.setAttribute('src', 'https://nytimes.github.io/svg-crowbar/svg-crowbar.js');
+	e.setAttribute('class', 'svg-crowbar');
+	document.body.appendChild(e);
 }
